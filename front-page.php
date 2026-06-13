1acbf89f70c1a54b56ce0d212416a416f80070c4
Hlushko Hrygorii <escagt82@gmail.com>
2026-06-13 18:30:42 +0300

    fix home-page

---
+++ front-page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<main class="container mx-auto px-4 py-8">
+	<article class="home_introduction">
+		<div class="container">
+			<div>
+				page-default
+			</div>
+			<div>
+				Home Page
+			</div>
+		</div>
+	</article>
+</main>
+
+<?php get_footer(); ?>
